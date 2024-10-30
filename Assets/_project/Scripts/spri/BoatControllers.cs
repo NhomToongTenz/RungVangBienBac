249d2bf45be8f1b7d255b5bd15117c91b1b8865a
NiehBright <169137767+NiehBright@users.noreply.github.com>
2024-10-30 23:06:13 +0700

    RaceBoat
    Đã xong phần thuyền tăng tốc khi điểm tăng

---
+++ Assets/_project/Scripts/spri/BoatControllers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    public float speed = 1f; // Tốc độ ban đầu của BoatController
+
+    // Hàm này để tăng tốc độ
+    public void IncreaseSpeed(float amount)
+    {
+        speed += amount;
+    }
+
+    // Ví dụ di chuyển sang phải dựa trên tốc độ
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
